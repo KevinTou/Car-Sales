a022ab0de1944a91962658da19a57762ac18e76f
Kevin Tou <toukevin@gmail.com>
2019-08-14 12:56:14 -0700

    Added an add feature action creator

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -2,14 +2,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 import AdditionalFeature from './AdditionalFeature';
 
+import { addFeature } from '../actions';
+
 const AdditionalFeatures = props => {
+  const buyItem = item => {
+    // dispatch an action here to add an item
+    props.addFeature(item);
+  };
+
   return (
     <div className='content'>
       <h4>Additional Features</h4>
       {props.store.length ? (
         <ol type='1'>
           {props.store.map(item => (
-            <AdditionalFeature key={item.id} feature={item} />
+            <AdditionalFeature key={item.id} feature={item} buyItem={buyItem} />
           ))}
         </ol>
       ) : (
@@ -27,5 +34,5 @@ const mapStateToProps = state => {
 
 export default connect(
   mapStateToProps,
-  {},
+  { addFeature },
 )(AdditionalFeatures);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -17,6 +17,14 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case 'ADD_FEATURE':
+      return {
+        ...state,
+        car: {
+          ...state.car,
+          features: [...state.car.features, action.payload],
+        },
+      };
     default:
       return state;
   }
